65d1a6d9afb5a1489696aee9ed4e1c94af6b14af
gaston-duault <61382435+gaston-duault@users.noreply.github.com>
2020-05-18 09:59:36 +0200

    maj classe Reservation/Client 18/05/2020

--- Programmtion/Camping/src/main/java/Camping/Reservation.java
+++ Programmtion/Camping/src/main/java/Camping/Reservation.java
@@ -1,6 +1,7 @@
 package Camping;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -15,10 +16,15 @@ public class Reservation {
     private int nbReservation = 0;
     private int numeroReservation;
     private Map<Client, Integer> mesres;
+    private ArrayList<Emplacement> mesEmplacements;
+    private final int MAX = 30;
 
-    public Reservation() {
+    public Reservation(Date dd , Date da) {
         nbReservation++;
         numeroReservation = nbReservation;
+        dateArrivee = da;
+        dateDepart = dd;
+        mesEmplacements = new ArrayList<Emplacement>(MAX);
     }
 
     public int getnbReservation() {
@@ -28,6 +34,10 @@ public class Reservation {
     public void ajouterReservation(Client client, int nbRes) {
         this.mesres = new HashMap<Client, Integer>();
         mesres.put(client, nbRes);
+    }
+
+    public void ajouterEmplacement(Emplacement e){
+        mesEmplacements.add(e);
     }
 
     /*
@@ -52,14 +62,33 @@ public class Reservation {
 
         if(mesres.containsKey(c)){
             mesres.remove(c);
+            mesEmplacements=null;
         }
         else{
             System.out.println("la réservation que vous voulez supprimer n'existe pas");
         }
     }
 
-    public void modifierReservation(Client client, int nbR){
+    public void modifierReservation(Client client, int nbR, Date dateD, Date dateA){
         mesres.put(client, nbR);
+        dateDepart = dateD;
+        dateArrivee = dateA;
+    }
+
+    public void supprimerEmplacement(int numeroEmplacement){
+        int i = 0;
+        boolean trouve = false;
+        while(i<=MAX || trouve==false){
+            if(numeroEmplacement==mesEmplacements.get(i).getNumeroEmp()){
+                trouve = true;
+            }
+        }
+        if (trouve==true){
+            mesEmplacements.remove(i);
+        }
+        else{
+            System.out.println("Nous n'avons pas trouvé l'emplacement que vous voulez supprimer");
+        }
     }
 
     public void afficher(){
@@ -69,8 +98,11 @@ public class Reservation {
         Map.Entry<Client, Integer> ent;
 
         while (it.hasNext()){
-            System.out.println(this.numeroReservation);
+            System.out.println(this.numeroReservation + dateDepart.toString() + dateArrivee.toString());
             client.afficher();
+        }
+        for (int i = 0; i < mesEmplacements.size(); i++) {
+            System.out.println(mesEmplacements.get(i).toString() + "\n");
         }
     }
 }
