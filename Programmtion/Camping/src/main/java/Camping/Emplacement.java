68269c2a03e03b1bc64be1515f273ad172397cd0
gaston duault <5672666-gastonduault@users.noreply.gitlab.com>
2020-06-19 08:11:32 +0000

    Update Emplacement.java

--- Programmtion/Camping/src/main/java/Camping/Emplacement.java
+++ Programmtion/Camping/src/main/java/Camping/Emplacement.java
@@ -4,30 +4,36 @@ import java.util.ArrayList;
 
 public class Emplacement {
 
-    private int numeroEmplacement;
+    private int numeroEmplacement=0;
     private static int nbEmplacement = 0;
     private double surface;
     private boolean estDispo;
     private int prixJour;
     private int nbVehicule;
+    private static final int MAX = 33;
 
     public static ArrayList<Emplacement> listeEmplacement = new ArrayList<Emplacement>();  
 
     // Contructeur
     public Emplacement(double surface, int prix, int vehicule){
         nbEmplacement++;
-        numeroEmplacement = nbEmplacement;
-    
+        numeroEmplacement=nbEmplacement;
         this.surface = surface;
         this.prixJour = prix;
         this.nbVehicule = vehicule;
         estDispo = true;
-        listeEmplacement.add(this);
     }
 
+    public static void creeremplacement(){
+        for(int i=0;i<MAX;i++){
+            Emplacement e = new Emplacement(20, 30, 1);
+            Emplacement.listeEmplacement.add(e);
+            FenSearchEmplacement.Liste.getItems().add(e.toString());
+        }
+    }
 
     // Guetteur
-    public int getNumeroEmp(){
+    public  int getNumeroEmp(){
         return numeroEmplacement;
     }
 
@@ -74,12 +80,20 @@ public class Emplacement {
         if (estDispo){
             dispo = "Oui";
         }
-        return "N° d'emplacement : " + this.numeroEmplacement + "\nSurface : " + this.surface + "\nPrix par Jour : " + this.prixJour
+        return "N° d'emplacement : " + numeroEmplacement + "\nSurface : " + this.surface + "\nPrix par Jour : " + this.prixJour
          + "\nNombre de véhicule : " + this.nbVehicule + "\nDisponible : " + dispo;
     }
 
-    public String toString(){
-        return "N°" + this.getNumeroEmp() + "\nSurface : " + this.surface + "\nPrix " + this.prixJour;
+    public String toString() {
+        return "N°" + getNumeroEmp();
+    }
+
+    public static void afficher(){
+        int i = 0;
+        while(i<=listeEmplacement.size()){
+            System.out.println(listeEmplacement.get(i).toString());
+            i++;
+        }
     }
 
 }
